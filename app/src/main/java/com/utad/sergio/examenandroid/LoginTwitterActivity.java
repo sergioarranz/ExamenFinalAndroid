0e2d557027b048839dde6738e56bd9f2d5c9343a
Sergio Arranz Sobrino <sergio.arranz@live.u-tad.com>
2018-02-19 09:39:44 +0100

    Creación LoginActivity & Nav Drawer

---
+++ app/src/main/java/com/utad/sergio/examenandroid/LoginTwitterActivity.java
@@ -0,0 +1,13 @@
+package com.utad.sergio.examenandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginTwitterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_twitter);
+    }
+}
